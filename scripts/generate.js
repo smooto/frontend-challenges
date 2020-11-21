2ab16d40c8c6c27fd50e860e1051fc7cda6a0843
smooto <melissasmoot@live.com>
2020-11-20 16:55:58 -0800

    Update generator script to return a random one-off challenge

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,7 +1,61 @@
 #!/usr/bin/env node
 
-function generate(type) {
-  console.log(`Generating ${type} challenge...`)
+const components = [
+  'breadcrumb trail',
+  'carousel',
+  'weather forecast widget',
+  'user information form',
+  'typography set',
+  'map viewer',
+  'media player',
+  'social feed',
+  'chat interface',
+  'content-sharing modal',
+  'tutorial wizard',
+  'data visualization',
+  'navigation bar or menu',
+  'dashboard'
+]
+
+const aesthetics = [
+  'flat/material',
+  'skeumorphic',
+  'neumorphic',
+  'hostile',
+  'brutalist',
+  'geocities-inspired',
+  'medieval',
+  'print-inspired',
+  'minimalist',
+  'maximalist',
+  'absurdist'
+]
+
+const twists = [
+  'animation',
+  'microinteractions',
+  '3D rendering',
+  'light mode/dark mode toggling',
+  'scrolling-responsive behaviors',
+  'cursor-tracking behaviors',
+  'graceful degradation',
+  'progressive enhancement',
+  'keyboard-oriented navigation'
+]
+
+function pickOneFrom(array) {
+  const idx = Math.floor(Math.random() * array.length)
+  return array[idx]
 }
 
-generate(process.argv[2])
+function generate() {
+  const aesthetic = pickOneFrom(aesthetics)
+  const component = pickOneFrom(components)
+
+  console.log(`
+    Your component challenge:\n
+    Build ${/[aeiou]/.test(aesthetic.charAt(0)) ? 'an' : 'a'} ${aesthetic} ${component}.
+  `)
+}
+
+generate()
