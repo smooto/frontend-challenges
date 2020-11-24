3abd8e4498652bc77454183db81b06fcb881f668
smooto <melissasmoot@live.com>
2020-11-24 13:06:24 -0800

    Init files and meta for challenge

---
+++ pages/aesthetic-challenges/11-24-20.js
@@ -0,0 +1,30 @@
+/*
+11-24-2020 challenge
+
+component: calculator
+aesthetic: neumorphism
+stretch: responds to keyboard inputs
+
+features:
+- operators: add, subtract, multiply, divide
+- equals and clear
+- display number currently being entered
+- on selecting operator...
+  - clear current number from view
+  - append number and operator to the total operation
+- on selecting "="...
+  - perform total operation
+  - display result
+
+other stuff:
+- write some tests to verify operations
+  - randomized tests would be a good idea
+*/
+
+export default function neumorphicCalculator() {
+  return (
+    <div>
+
+    </div>
+  )
+}
