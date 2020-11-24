0f9926f8c1f4a53899234b33c44ab261885b3697
smooto <melissasmoot@live.com>
2020-11-24 13:20:35 -0800

    Validate routing and stylesheet setup

--- pages/component-challenges/11-24-20.js
+++ pages/component-challenges/11-24-20.js
@@ -19,12 +19,15 @@ features:
 other stuff:
 - write some tests to verify operations
   - randomized tests would be a good idea
+
+http://localhost:3000/component-challenges/11-24-20
 */
+import styles from './11-24-20.module.css'
 
 export default function neumorphicCalculator() {
   return (
-    <div>
-
+    <div className={styles.container}>
+      <p>Hello, world!</p>
     </div>
   )
 }
