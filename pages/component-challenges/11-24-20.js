99dff265df8e67c932357a14ead05f7a0bbacc7d
smooto <melissasmoot@live.com>
2020-11-24 13:56:48 -0800

    Add basic elements and structure

--- pages/component-challenges/11-24-20.js
+++ pages/component-challenges/11-24-20.js
@@ -26,8 +26,28 @@ import styles from './11-24-20.module.css'
 
 export default function neumorphicCalculator() {
   return (
-    <div className={styles.container}>
-      <p>Hello, world!</p>
+    <div id="container" className={styles.container}>
+      <div id="screen" className={styles.screen}>
+        {/* TODO: number displays */}
+      </div>
+      <div className={styles.buttonsGrid}>
+        <button className={styles.clearButton}>clear</button>
+        <button>=</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>x</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>/</button>
+        {/* <button>0</button> */}
+      </div>
     </div>
   )
 }
